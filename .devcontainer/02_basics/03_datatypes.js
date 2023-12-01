709eca360bdc301326980c6a4715741746e85de7
mrshivsharma <152585900+mrshivsharma@users.noreply.github.com>
2023-12-01 08:24:53 +0000

    datatypes

---
+++ .devcontainer/02_basics/03_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+//alert(4+4); we are useing nodejs, not browser
+
+console.log(3
+    +3); //code readablity should be high
+
+    console.log("shiv");
