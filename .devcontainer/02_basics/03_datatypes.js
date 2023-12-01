38996435bef7ff8aeeccc6133255683026c55e47
mrshivsharma <152585900+mrshivsharma@users.noreply.github.com>
2023-12-01 09:45:46 +0000

    datatypes

--- .devcontainer/02_basics/03_datatypes.js
+++ .devcontainer/02_basics/03_datatypes.js
@@ -3,6 +3,54 @@
 //alert(4+4); we are useing nodejs, not browser
 
 console.log(3
-    +3); //code readablity should be high
+   +3); //code readablity should be high
 
     console.log("shiv");
+
+    let score = "shiv"
+
+    console.log(typeof score);
+    let valuInNumber = Number(score)
+    console.log(valuInNumber);
+    console.log(valuInNumber)
+
+    //"44" = 44
+    //true => 1; false => 0
+   // 55dfe = not a number
+
+    let isLoggedIn = 1
+
+    let booleanisLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanisLoggedIn);
+
+    let somenumber = 55
+
+    let stringnumber = String (somenumber)
+    console.log(stringnumber)
+    console.log(typeof stringnumber)
+
+    // chek for negitive valu 
+
+     let valu = 5
+     let negvalu = -valu
+     console.log(negvalu);
+
+     // add by string
+     let str1 = "hello"
+     let str2 = " shiv"
+
+     let str3 = str1 + str2
+     console.log(str3);
+
+     // chek for number add
+
+    console.log(2 + 4);
+    console.log(8 * 9);
+    console.log("3" + 4);
+    console.log(3 + 6 + "9");
+
+    //increment operetor i++, ++i ;
+
+    let gameCounter = 100
+    ++gameCounter;
+    console.log(gameCounter);
